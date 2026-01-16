5908c58acd5c3e4583bebf2889b2c9087cf0aabc
ttn54 <ttn54@sfu.ca>
2026-01-15 21:06:21 -0800

    Remove unused imports

--- frontend/src/components/Calendar/WeeklyCalendar.tsx
+++ frontend/src/components/Calendar/WeeklyCalendar.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState, useEffect, useRef } from 'react';
+import React, { useMemo, useState } from 'react';
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
 import { format, parse, startOfWeek, getDay } from 'date-fns';
 import { enUS } from 'date-fns/locale/en-US';
